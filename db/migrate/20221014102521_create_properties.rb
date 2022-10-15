7405f6c3c6c85c099abe79e6ad69970730c02509
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-16 01:19:24 +0530

    add null constraint

--- db/migrate/20221014102521_create_properties.rb
+++ db/migrate/20221014102521_create_properties.rb
@@ -1,7 +1,7 @@
 class CreateProperties < ActiveRecord::Migration[7.0]
   def change
     create_table :properties do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_102703) do
   end
 
   create_table "properties", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_properties_on_name", unique: true
