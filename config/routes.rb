9a391bbcb4279f687dd8322fc614cb2f3694e356
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-18 02:08:16 +0530

    updated url- products nested in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "products#index"
   devise_for :users
-  resources :products
+
+  resources :user do
+    resources :products, only: [:index, :new, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
