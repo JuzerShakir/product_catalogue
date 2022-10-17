7c5df56eced285d4523271a6dd96fe98b41c4bb4
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-17 17:02:35 +0530

    no more separate pluck queries for each property of a product, using lazy load to the advantage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,16 @@ module ApplicationHelper
         # link_to(name, "#", class: 'add_fields', data: {id: id, fields: fields.gsub("\n", "") } )
 
     end
+
+    def properties_listing_of product
+        prod_value = product.product_properties.pluck(:value)
+        prod_name = product.properties.pluck(:name)
+        properties = prod_name.zip(prod_value)
+
+        output = ""
+        properties.each do | property |
+            output << property.join(':') << ", "
+        end
+        output.delete_suffix(', ')
+    end
 end
