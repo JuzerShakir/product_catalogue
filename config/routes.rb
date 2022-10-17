700c5972ef6c95ef4166bf97ef749d5042c0454b
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-18 00:43:40 +0530

    bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "products#new"
+  root to: "products#index"
   devise_for :users
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
