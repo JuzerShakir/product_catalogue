706311b1d36d7a50dbb40e93fb62ca090ee7af36
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-17 19:46:38 +0530

    remove validator folder

--- app/models/validators/custom_length_validator.rb
+++
@@ -1,7 +0,0 @@
-class CustomLengthValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-        unless value.match?(/^\d{12,13}$|^\d{10}$/)
-            record.errors.add(attribute, "length is not within range")
-        end
-    end
-end
