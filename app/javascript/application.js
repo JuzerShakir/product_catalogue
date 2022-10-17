c4ce357ae33249d578752fb2d49437ae339e6aab
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-17 19:44:43 +0530

    remove unnecessary js code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,22 +3,3 @@ import "@hotwired/turbo-rails";
 import "controllers";
 import "jquery";
 import "@nathanvda/cocoon";
-
-// $("form").on("click", ".add_fields", function (event) {
-//   let regexp, time;
-//   time = new Date().getTime();
-//   regexp = new RegExp($(this).data("id"), "g");
-//   $(".fields").append($(this).data("fields").replace(regexp, time));
-//   return event.preventDefault();
-// });
-
-// const add_fields = document.querySelector(".add_fields");
-// const fields = document.querySelector(".fields");
-
-// add_fields.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   // console.log("Hi");
-//   const time = new Date().getTime();
-//   const regexp = new RegExp($(this).data("id"), "g");
-//   fields.append(this.data("fields").replace(regexp, time));
-// });
