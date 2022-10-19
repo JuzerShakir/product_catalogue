d630b906ccf0ad82ed891d3e4e7cebbf61834fb4
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-19 14:19:35 +0530

    remove uniqueness constraint

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_102703) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_properties_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
